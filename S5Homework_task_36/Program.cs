b490179656639038fab6484ecaab10f27b4630dc
AntonZozulya <antonzo93@mail.ru>
2023-03-31 16:35:37 +0300

    add task_36

---
+++ S5Homework_task_36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100); // [-100, 99]
+}
+
+int SumOddNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (i % 2 != 0 )
+      
+      sum += array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма эл-ов,наход. на нечетных индексах:{SumOddNumbers(array)}");
